3a7742ee26e9232fe358cf112c53ed23bc617fe1
Ihar-Zaharenko <igorza95@mail.ru>
2020-02-16 16:29:17 +0300

    Zadati Bazovi

---
+++ Zadata13.java
@@ -0,0 +1,11 @@
+public class Zadata13 {
+    public static void main(String[] arqs) {
+        int x=2;
+        int y=10;
+        int n=0;
+        for(int i=x; i<=y ;i++){
+            n++;
+        }
+            System.out.println("К числу x нужно прибавить  "+n+" что бы оно было больше или равно у");
+        }
+    }
